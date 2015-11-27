4e24940b1d98c8e9c97cb92dc69be8d3e69736a7
Jesse Soldat <jsoldat@hotmail.com>
2015-11-27 11:13:36 -0500

    check for valid an @ symbol in the email and check for either http:// or https:// in the website area

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,14 @@ _angular2['default'].module('app', []).controller('FormController', function ($s
 
   vm.addContact = addContact;
 
+  $scope.$watch('contact.email', function (newVal) {
+    if (!newVal) return;
+
+    if (!validateEmail(newVal)) {
+      return console.log('email needs an @ symbol');
+    }
+  });
+
   function addContact(contactObj) {
     // console.log(contactObj);
 
@@ -28,8 +36,14 @@ _angular2['default'].module('app', []).controller('FormController', function ($s
     if (!validateEmpty(contactObj.email)) {
       return console.log('email is empty');
     }
+    if (!validateEmail(contactObj.email)) {
+      return console.log('email must be valid');
+    }
     if (!validateEmpty(contactObj.website)) {
       return console.log('website is empty');
+    }
+    if (!validateWebsite(contactObj.website)) {
+      return console.log('website needs to be valid');
     }
     if (!validateEmpty(contactObj.message)) {
       return console.log('text area is empty');
@@ -38,6 +52,15 @@ _angular2['default'].module('app', []).controller('FormController', function ($s
 
   function validateEmpty(field) {
     return field ? true : false;
+  }
+
+  function validateEmail(field) {
+    return field.indexOf('@') >= 0 ? true : false;
+  }
+
+  function validateWebsite(field) {
+    var pattern = /^https?:\/\//i;
+    return pattern.test(field);
   }
 });
 //controller
--- js/main.js
+++ js/main.js
@@ -8,6 +8,15 @@ angular
 
     vm.addContact = addContact;
 
+    $scope.$watch('contact.email', function(newVal) {
+      if(!newVal) return;
+
+      if(!validateEmail(newVal)) {
+        return console.log('email needs an @ symbol');
+      }
+
+    })
+
     function addContact(contactObj) {
       // console.log(contactObj);
 
@@ -24,8 +33,14 @@ angular
       if(!validateEmpty(contactObj.email)) {
         return console.log('email is empty');
       }
+      if(!validateEmail(contactObj.email)) {
+        return console.log('email must be valid');
+      }
       if(!validateEmpty(contactObj.website)) {
         return console.log('website is empty');
+      }
+      if(!validateWebsite(contactObj.website)) {
+        return console.log('website needs to be valid');
       }
       if(!validateEmpty(contactObj.message)) {
         return console.log('text area is empty');
@@ -33,11 +48,17 @@ angular
 
     }
 
-
-
-
     function validateEmpty(field) {
       return field ? true : false;
+    }
+
+    function validateEmail(field) {
+      return (field.indexOf('@') >= 0) ? true : false;
+    }
+
+    function validateWebsite(field) {
+      var pattern = /^https?:\/\//i;
+      return pattern.test(field);
     }
 
 
